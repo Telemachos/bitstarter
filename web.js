e6a09c2a2694c659b25db8c12a4c39137aea8feb
Telemachos <rgwsmeenk@yahoo.co.uk>
2013-07-06 13:43:52 +0000

    added index tekst

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var bufferTekst = fs.readFileSync('index.html');
+  var tekst = bufferTekst.buf.toString();
+  response.send(tekst);
 });
 
 var port = process.env.PORT || 5000;
